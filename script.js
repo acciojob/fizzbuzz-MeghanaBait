bb4c6fdc2fa37500bccc858e6ed5d9a584ef8a16
MeghanaBait <133511757+MeghanaBait@users.noreply.github.com>
2023-12-19 10:54:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+let ans = "";
+
+for(let i = 1; i <=20; i++){
+	if(i % 3 === 0 && i % 5 === 0){
+		ans += "fizzBuzz\n";
+	}else if(i % 3 === 0){
+		ans +="fizz\n";
+	}else if(i % 5 === 0){
+		ans += "Buzz\n";
+	}else{
+		ans += i+"\n";
+	}
+}
+
+alert(output);
